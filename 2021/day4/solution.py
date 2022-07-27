af6cdda7de8f5d555eece9265a6eafb94e0a871d
Kamoliddin1 <chkb007147@gmail.com>
2022-07-27 20:59:48 +0500

    day4

--- 2021/day4/solution.py
+++ 2021/day4/solution.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from typing import List, Tuple
 
 INPUTS_FILE = Path(__file__).parent / 'input.txt'
-print(INPUTS_FILE)
 
 
 def parse(input_str: str) -> Tuple[List[List[List[int]]], List[int]]:
@@ -18,17 +17,51 @@ def parse(input_str: str) -> Tuple[List[List[List[int]]], List[int]]:
     return boards, draws
 
 
+def check_bingo(board, history) -> bool:
+    for row in board:
+        if all(elem in history for elem in row):
+            return True
+    for column in zip(*board):
+        if all(elem in history for elem in column):
+            return True
+    return False
+
+
+def sum_them(board, history) -> int:
+    return sum(elem for row in board for elem in row if elem not in history)
+
+
+def calc_part1(input_str: str) -> int:
+    boards, draws = parse(input_str)
+    for i in range(1, len(draws)):
+        history = draws[:i]
+        for board in boards:
+            if check_bingo(board, history):
+                summed = sum_them(board, history)
+                return summed * history[-1]
+
+    raise ValueError("Cannot find an answer")
+
+
+def calc_part2(input_str: str) -> int:
+    boards, draws = parse(input_str)
+
+    for i in range(1, len(draws)):
+        history = draws[:i]
+
+        if len(boards) == 1 and check_bingo(boards[0], history):
+            return sum_them(boards[0], history) * history[-1]
+
+        boards = [board for board in boards if not check_bingo(board, history)]
+
+    raise ValueError("Cannot find an answer")
+
+
 def main() -> None:
     with INPUTS_FILE.open() as fp:
         input_str = fp.read()
-        boards, draws = parse(input_str)
-        items = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
-        print(len(items))
-        for i in range(5):
-            for j in range(5):
-                for k in range(5):
-                    item = [items[i][j][k]]
-
+        print(calc_part1(input_str))
+        print(calc_part2(input_str))
 
 
 if __name__ == "__main__":
